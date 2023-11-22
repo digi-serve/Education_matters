ab657ff3af4eb1a7606203525a554f8d66cde2ec
Prin Techarong <techarong.prin@gmail.com>
2023-11-22 16:53:00 +0700

    Fixed_Test

--- 0001_education_matters_addChild.cy.js
+++ 0001_education_matters_addChild.cy.js
@@ -26,39 +26,39 @@ beforeEach(() => {
 describe("Adding Child", () => {
    it("Can Add New Child", () => {
       cy.get(
-         "[data-cy='menu-item Add Child 04e92328-43c4-4f9b-ba57-b71c71db9570 41cee2d7-e3f8-4837-bfc2-084efe2a3883']"
+         "[data-cy='menu-item Add Child f9c62acf-9d2f-433b-b9dd-aa3060b0eb9d 56c95826-8f2a-401a-accf-949677fda0be']"
       )
          .should("exist")
          .click();
       cy.get(
-         "[data-cy='string First Name db4a4e5b-0b41-477a-a750-bacaa9309e10 ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='string First Name db4a4e5b-0b41-477a-a750-bacaa9309e10 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       )
          .click()
          .type("Johnny");
       cy.get(
-         "[data-cy='string Last Name 9745668a-d3f6-4c84-a282-f115499dee8b ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='string Last Name 9745668a-d3f6-4c84-a282-f115499dee8b 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       )
          .click()
          .type("walker");
       cy.get(
-         "[data-cy='string Nick Name ed84ddf8-cf83-4349-a893-40753cdef614 ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='string Nick Name ed84ddf8-cf83-4349-a893-40753cdef614 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       )
          .click()
          .type("John");
       cy.get(
-         "[data-cy='date Date of Birth 9b9af619-5db9-489a-81c0-9ff317337352 ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='date Date of Birth 9b9af619-5db9-489a-81c0-9ff317337352 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       )
          .click()
          .type("13/08/2012")
          .type("{enter}");
       cy.get(
-         "[data-cy='list Gender 5032517e-9771-4133-8108-d2b96a83e4a3 ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='list Gender 5032517e-9771-4133-8108-d2b96a83e4a3 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       ).click();
       cy.get(
-         "[data-cy='list options Male 5032517e-9771-4133-8108-d2b96a83e4a3 ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='list options Male 5032517e-9771-4133-8108-d2b96a83e4a3 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       ).click();
       cy.get(
-         "[data-cy='button save ed19aa55-4fe1-4be0-93a2-c4b1fb737641']"
+         "[data-cy='button save 36ec27e9-85d6-4e34-9d20-98c65988b0e8']"
       ).click();
    });
 });
